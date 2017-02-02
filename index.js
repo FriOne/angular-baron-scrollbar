b318f6e1e419838b91d592f2688a4fdec7396328
Lyubimov Roman <lubimov@brooma.ru>
2017-02-02 15:32:17 +0700

    refactor a little, add on scroll callbacks

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ require('baron/baron.min.js');  // creates window.baron object
 module.exports = 'angular-baron-scrollbar';
 
 var template =
-' <div class="scroller_wrapper" > ' +
-'   <div class="scroller" in-view-container  ng-transclude></div>           ' +
-'   <div class="scroller__track_v">                                         ' +
-'     <div class="scroller__bar_v"></div>                                   ' +
-'   </div>                                                                  ' +
-'   <div class="scroller__track_h">                                         ' +
-'     <div class="scroller__bar_h"></div>                                   ' +
-'   </div>                                                                  ' +
-' </div>                                                                    ' ;
+  ' <div class="scroller_wrapper"> ' +
+  '   <div class="scroller" in-view-container ng-transclude></div>' +
+  '   <div class="scroller__track_v">                             ' +
+  '     <div class="scroller__bar_v"></div>                       ' +
+  '   </div>                                                      ' +
+  '   <div class="scroller__track_h">                             ' +
+  '     <div class="scroller__bar_h"></div>                       ' +
+  '   </div>                                                      ' +
+  ' </div>                                                        ' ;
 
 var defaultOpts = {
   scroller: '.scroller',
@@ -28,52 +28,123 @@ var defaultOpts = {
 
   // Local copy of jQuery-like utility
   // Default: window.jQuery
-  $: function(selector, context) {
+  $: window.jQuery || function(selector, context) {
     return angular.element(qwery(selector, context));
-  },
+  }
 };
 
-angular.module(module.exports, [])
-.directive('baronScrollbar', ['$parse', function ($parse) {
+angular
+  .module(module.exports, [])
+  .directive('baronScrollbar', ['$parse', controller]);
+
+function controller($parse) {
   return {
     restrict: 'EA',
     transclude: true,
     scope: {
-      update: '='
+      update: '=?',
+      moveToBottom: '=?',
+      topIndent: '<?',
+      bottomIndent: '<?',
+      toBottomOnInit: '<?',
+      onTop: '=?',
+      onBottom: '=?',
+      onScroll: '&?',
+      onScrollToTop: '&?',
+      onScrollToBottom: '&?',
+      direction: '@?'
     },
     template: template,
     replace: true,
-    link: function (scope, elem, attr) {
-      var hscroll, vscroll;
-      var el = elem[0];
-      var opts = $parse(attr['opts'])(scope);
-      var hopts = $parse(attr['hopts'])(scope);
-      var direction = attr['direction'] || 'y';
-      opts = xtend(defaultOpts, {root: el}, opts);
+    link: link
+  };
 
-      if (direction.indexOf('x') !== -1) {
-        hscroll = baron(xtend(opts, {
+  function link($scope, $element, attr) {
+    var hscroll, vscroll;
+    var moveToBottomInterval;
+    var opts = $parse(attr['opts'])($scope) || {};
+    var hopts = $parse(attr['hopts'])($scope) || {};
+    var direction = attr['direction'] || 'y';
+    opts = xtend(defaultOpts, {root: $element}, opts);
+
+    var $scroller = opts.$('.scroller', $element);
+    $scope.onTop = $scope.onTop || true;
+    $scope.onBottom = $scope.onBottom || false;
+    $scope.topIndent = $scope.topIndent || 100;
+    $scope.bottomIndent = $scope.bottomIndent || 0;
+    $scroller.on('scroll', onScroll);
+
+    if (direction.indexOf('x') !== -1) {
+      hscroll = baron(
+        xtend(opts, {
           barOnCls: 'baron_h',
           bar: '.scroller__bar_h',
           direction: 'h'
-        }, hopts));
-      }
-      if (direction.indexOf('y') !== -1) {
-        vscroll = baron(opts);
-      }
-
-      scope.$watch('update', function (n, o) {
-        if (n) {
-          hscroll && hscroll.update();
-          vscroll && vscroll.update();
-          scope.update = false;
-        }
-      });
-
-      scope.$on('$destroy', function () {
-        hscroll && hscroll.dispose();
-        vscroll && vscroll.dispose();
-      });
+        }, hopts)
+      );
     }
-  };
-}]);
+    if (direction.indexOf('y') !== -1) {
+      vscroll = baron(opts);
+    }
+
+    if ($scope.toBottomOnInit) {
+      // Little hack to scroll to bottom on init immediately after full render.
+      moveToBottomInterval = setInterval(function() {
+        if (!isOnBottom()) {
+          moveToBottom();
+          clearInterval(moveToBottomInterval);
+        }
+      }, 60);
+    }
+
+    $scope.$watch('update', onUpdate);
+    $scope.$watch('moveToBottom', onMoveToBottom);
+    $scope.$on('$destroy', onDestroy);
+
+    function onDestroy() {
+      clearInterval(moveToBottomInterval);
+      $scroller.off('scroll');
+      hscroll && hscroll.dispose();
+      vscroll && vscroll.dispose();
+    }
+
+    function onUpdate(n, o) {
+      if (n) {
+        hscroll && hscroll.update();
+        vscroll && vscroll.update();
+        $scope.update = false;
+      }
+    }
+
+    function onMoveToBottom(n, o) {
+      if (n) {
+        $scope.moveToBottom = false;
+        moveToBottom();
+      }
+    }
+
+    function onScroll() {
+      var prevOnBottom = $scope.onBottom;
+      var prevOnTop = $scope.onTop;
+      $scope.onTop = ($scroller[0].scrollTop <= $scope.topIndent);
+      $scope.onBottom = isOnBottom();
+
+      $scope.onScroll && $scope.onScroll();
+      if ($scope.onBottom && prevOnBottom !== $scope.onBottom && $scope.onScrollToBottom) {
+        $scope.onScrollToBottom();
+      }
+      if ($scope.onTop && prevOnTop !== $scope.onTop && $scope.onScrollToTop) {
+        $scope.onScrollToTop();
+      }
+    }
+
+    function isOnBottom() {
+      return (($scroller[0].scrollHeight - $scroller[0].scrollTop - $scope.bottomIndent) <= $scroller[0].clientHeight);
+    }
+
+    function moveToBottom() {
+      $scroller[0].scrollTop = $scroller[0].scrollHeight;
+      self.onBottom = true;
+    }
+  }
+}
